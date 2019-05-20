e9051e0658156390c6a80949e5c9b9bd8a79b64c
Agik <agbaezeemmanuel@gmail.com>
2019-05-20 10:39:44 +0100

    added  POST method for adding url, GET for redirecting to url

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+var store = [];
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function(req, res, next) {
+  res.render("index", { title: "Express", store });
 });
 
+router.post("/api/shorturl/new", function(req, res, next) {
+  let url = req.body.url;
+  store.push(url);
+  let toStore = { "original url": url, "short url": store.length };
+  res.json(toStore);
+});
+
+router.get("/api/shorturl/:id", function(req, res, next) {
+  let { id } = req.params;
+  let url = store[id - 1] || "/api/error";
+  res.redirect(301, url);
+});
+
+router.get("/api/error", function(req, res, next) {
+  let error = "Invalid url";
+  res.json({ error });
+});
 module.exports = router;
