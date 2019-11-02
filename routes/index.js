fdce7084404fcb7b611b2653a92768b562e0f2be
Agik <agbaezeemmanuel@gmail.com>
2019-11-02 12:54:33 +0100

    writing the API
    API for the url-shortener

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,29 @@
 var express = require("express");
+var url = require("url");
+var dns = require("dns");
 var router = express.Router();
 var store = [];
+
+// Check if the URL exists
+const isValid = x => {
+  let hostname = url.parse(x).hostname;
+  dns.lookup(hostname, (err, address) => {
+    if (address) {
+      return x;
+    } else {
+      return null;
+    }
+  });
+};
+// function isValidUrl(req, res, next) {
+//   let { url } = req.body;
+//   if (!isValid(url)) {
+//     let error = "Url does not exist";
+//     res.json({ error });
+//   } else {
+//     next();
+//   }
+// }
 
 /* GET home page. */
 router.get("/", function(req, res, next) {
@@ -8,10 +31,19 @@ router.get("/", function(req, res, next) {
 });
 
 router.post("/api/shorturl/new", function(req, res, next) {
-  let url = req.body.url;
-  store.push(url);
-  let toStore = { "original url": url, "short url": store.length };
-  res.json(toStore);
+  var url = isValid(req.body.url);
+  // if (!isValid(url)) {
+  //   let error = "Url does not exist!";
+  //   res.json({ error });
+  // } else {
+  if (url == null) {
+    let error = "Url does not exist!";
+    res.json({ error });
+  } else {
+    store.push(url);
+    let toStore = { "original url": url, "short url": store.length };
+    res.json(toStore);
+  }
 });
 
 router.get("/api/shorturl/:id", function(req, res, next) {
@@ -24,4 +56,5 @@ router.get("/api/error", function(req, res, next) {
   let error = "Invalid url";
   res.json({ error });
 });
+
 module.exports = router;
